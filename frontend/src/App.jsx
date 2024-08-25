20ff88a840510e2997ed3e0b757aab5f6918caf5
StefanHritcu <shritcu@proton.me>
2024-08-25 22:53:05 +0200

    ste-add: ho implementato la root con i stili principali che useremo nel progetto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-
 import ChiSiamo from "./components/chisiamo/ChiSiamo";
 import Contatti from "./components/contatti/Contatti";
 import Header from "./components/header/Header";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { HelmetProvider } from "react-helmet-async";
 
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import "./header.css";
 
 function Header() {
   return (
