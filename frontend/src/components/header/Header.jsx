17d135550af2bd36dc1a393f7904ab3bda9226ff
Samuele <samueletasciotti@icloud.com>
2024-08-26 22:47:09 +0200

    samu-add: "Nav bar"

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,30 +1,26 @@
 import { Link } from "react-router-dom";
 
+
 function Header() {
   return (
     <>
-
-    {/* Div Contenitore di Bootstrap (Fluid prende in automatico la larghezza dello schermo). */}
-    <div className="container-fluid">
-
-      {/* E' la riga della pagina dove si possono inserire più COL */}
-      <div className="row">
-
-        {/* Col è la colonna dove vi è l'elemento interno */}
-        <div className="col">
-
-          {/* Con le varie classi vado a dare sia (Display: flex /// sia  Justify-comtent: space-around) */}
-          <header className="header d-flex justify-content-around">
-            <h1>Logo</h1>
-              <div>
+      {/* Div Contenitore di Bootstrap (Fluid prende in automatico la larghezza dello schermo). */}
+      <div className="container-fluid header">
+        {/* E' la riga della pagina dove si possono inserire più COL */}
+        <div className="row">
+          {/* Col è la colonna dove vi è l'elemento interno */}
+          <div className="col">
+            {/* Con le varie classi vado a dare sia (Display: flex /// sia  Justify-comtent: space-around) */}
+            <header className="header d-flex justify-content-between align-items-center p-3">
+              <h1>Logo</h1>
+              <div className="d-flex gap-3">
                 <Link to="/chisiamo">Chi Siamo</Link>
                 <Link to="/contatti">Contatti</Link>
               </div>
-          </header>
+            </header>
+          </div>
         </div>
       </div>
-    </div>
-      
     </>
   );
 }
