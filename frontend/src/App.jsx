8ca92589dfaa018a2a6748c4dddaeda07d7ac4a5
StefanHritcu <shritcu@proton.me>
2024-08-26 22:49:58 +0200

    ste-add: footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,24 @@ import Header from "./components/header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { HelmetProvider } from "react-helmet-async";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <HelmetProvider>
         <Header />
-        <Routes>
-          <Route path="/chisiamo" element={<ChiSiamo />} />
-          <Route path="/contatti" element={<Contatti />} />
-        </Routes>
+
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/chisiamo" element={<ChiSiamo />} />
+            <Route path="/contatti" element={<Contatti />} />
+          </Routes>
+        </main>
+
+        <Footer />
       </HelmetProvider>
-    </>
+    </div>
   );
 }
 
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <>
+      <footer className="$indigo-700">
+        <h1>Footer</h1>
+      </footer>
+    </>
+  );
+}
+export default Footer;
