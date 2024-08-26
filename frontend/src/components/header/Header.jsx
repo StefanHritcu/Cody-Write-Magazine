a96e046275596c2f0514a8ddb87a66121e34741f
Samuele <samueletasciotti@icloud.com>
2024-08-26 22:23:07 +0200

    samu-add: "Container / Row / Col di Bootstrap"

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,13 +3,28 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header className="header">
-        <h1>Logo</h1>
-        <div>
-          <Link to="/chisiamo">Chi Siamo</Link>
-          <Link to="/contatti">Contatti</Link>
+
+    {/* Div Contenitore di Bootstrap (Fluid prende in automatico la larghezza dello schermo). */}
+    <div className="container-fluid">
+
+      {/* E' la riga della pagina dove si possono inserire più COL */}
+      <div className="row">
+
+        {/* Col è la colonna dove vi è l'elemento interno */}
+        <div className="col">
+
+          {/* Con le varie classi vado a dare sia (Display: flex /// sia  Justify-comtent: space-around) */}
+          <header className="header d-flex justify-content-around">
+            <h1>Logo</h1>
+              <div>
+                <Link to="/chisiamo">Chi Siamo</Link>
+                <Link to="/contatti">Contatti</Link>
+              </div>
+          </header>
         </div>
-      </header>
+      </div>
+    </div>
+      
     </>
   );
 }
