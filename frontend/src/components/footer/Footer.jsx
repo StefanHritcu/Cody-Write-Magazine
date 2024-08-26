977e7be3ae490cc5b339c0105a4bc00934dcb5a9
StefanHritcu <shritcu@proton.me>
2024-08-27 00:29:33 +0200

    ste-add: aggiunta link esterni e interni del footer per ora solo per mobile

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,8 +1,108 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Link } from "react-router-dom";
+
 function Footer() {
+  const handleFacebookClick = () => {
+    alert(
+      "Se i profili FACEBOOK non si aprono, assicurati che i popup siano abilitati nel tuo browser."
+    );
+    window.open(
+      "https://www.facebook.com/profile.php?id=61563101911367",
+      "_blank"
+    );
+    window.open(
+      "https://www.facebook.com/profile.php?id=61563101911367",
+      "_blank"
+    );
+  };
+
+  const handleLinkedinClick = () => {
+    alert(
+      "Se i profili LINKEDIN non si aprono, assicurati che i popup siano abilitati nel tuo browser."
+    );
+    window.open(
+      "https://www.linkedin.com/in/stefan-florian-hritcu-ba615431b/",
+      "_blank"
+    );
+    window.open(
+      "https://www.linkedin.com/in/samuele-tasciotti-212b232a7/",
+      "_blank"
+    );
+  };
+
+  const handleGithubClick = () => {
+    alert(
+      "Se i profili GITHUB non si aprono, assicurati che i popup siano abilitati nel tuo browser."
+    );
+    window.open("https://github.com/StefanHritcu", "_blank");
+    window.open("https://github.com/Samueletasciotti00", "_blank");
+  };
+
+  const handleEmailClick = () => {
+    alert(
+      "Se i profili GITHUB non si aprono, assicurati che i popup siano abilitati nel tuo browser."
+    );
+    window.open("mailto:stefano.94h@gmail.com", "_blank");
+    window.open("mailto:stefano.94h@gmail.com", "_blank");
+  };
+
   return (
     <>
-      <footer className="$indigo-700">
-        <h1>Footer</h1>
+      <footer className="mt-auto py-4">
+        {/* LINKS ESTERNI SOCIAL */}
+        <nav className="d-flex justify-content-around align-items-center">
+          <a
+            onClick={handleFacebookClick}
+            aria-label="Facebook"
+            role="button"
+            title="Facebook"
+            className="icon-size"
+          >
+            <i className="bi bi-facebook"></i>
+          </a>
+          <a
+            onClick={handleLinkedinClick}
+            aria-label="Linkedin"
+            role="button"
+            title="Linkedin"
+            className="icon-size"
+          >
+            <i className="bi bi-linkedin"></i>
+          </a>
+          <a
+            onClick={handleGithubClick}
+            aria-label="Github"
+            role="button"
+            title="Github"
+            className="icon-size"
+          >
+            <i className="bi bi-github"></i>
+          </a>
+          <a
+            onClick={handleEmailClick}
+            aria-label="Github"
+            role="button"
+            title="Github"
+            className="icon-size"
+          >
+            <i className="bi bi-envelope"></i>
+          </a>
+        </nav>
+        {/* LINKS INTERNI SITO */}
+        <main className="container d-flex justify-content-around align-items-center my-4">
+          <div className="row">
+            <div className="d-flex flex-column">
+              <Link>Articoli</Link>
+              <Link>Categorie</Link>
+            </div>
+          </div>
+          <div className="row">
+            <div className="d-flex flex-column">
+              <Link>Articoli</Link>
+              <Link>Categorie</Link>
+            </div>
+          </div>
+        </main>
       </footer>
     </>
   );
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,29 +1,18 @@
 import { Link } from "react-router-dom";
 
-
 function Header() {
   return (
     <>
       {/* Div Contenitore di Bootstrap (Fluid prende in automatico la larghezza dello schermo). */}
-<<<<<<< HEAD
       <div className="container-fluid">
-=======
-      <div className="container-fluid header">
->>>>>>> 17d135550af2bd36dc1a393f7904ab3bda9226ff
         {/* E' la riga della pagina dove si possono inserire più COL */}
         <div className="row">
           {/* Col è la colonna dove vi è l'elemento interno */}
           <div className="col">
             {/* Con le varie classi vado a dare sia (Display: flex /// sia  Justify-comtent: space-around) */}
-<<<<<<< HEAD
             <header className="header d-flex justify-content-around">
               <h1>Logo</h1>
               <div>
-=======
-            <header className="header d-flex justify-content-between align-items-center p-3">
-              <h1>Logo</h1>
-              <div className="d-flex gap-3">
->>>>>>> 17d135550af2bd36dc1a393f7904ab3bda9226ff
                 <Link to="/chisiamo">Chi Siamo</Link>
                 <Link to="/contatti">Contatti</Link>
               </div>
